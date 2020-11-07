ce8d0ef2572cb151bf480ab332e4a1a659b3791c
GustavoBlaze <gustavoblaze1524@gmail.com>
2020-11-07 15:52:06 -0300

    feat: create a header for application

---
+++ src/components/Header/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import logo from "../../assets/img/logo.png";
+import "./styles.scss";
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="header__container">
+        <img src={logo} alt="objective logo" />
+
+        <div className="candidate">
+          <b>Luiz Gustavo</b>
+          <p>Teste de Front-end</p>
+        </div>
+
+        <strong className="letters">CB</strong>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React from "react";
+
+import Header from "../../components/Header";
 
 function Home() {
-  return <h1>Home!</h1>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default Home;
