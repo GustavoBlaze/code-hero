e37ca8934d14d454fd9cd99440e75745c8663852
GustavoBlaze <gustavoblaze1524@gmail.com>
2020-11-07 17:37:53 -0300

    feat(Home): add SearchForm and apply responsiveness

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,19 @@
 import React from "react";
 
 import Header from "../../components/Header";
+import SearchForm from "../../components/SearchForm";
+
+import "./styles.scss";
 
 function Home() {
   return (
-    <>
+    <div className="page">
       <Header />
-    </>
+      <section className="page__content">
+        <h1 className="page__content__title">Busca de personagens</h1>
+        <SearchForm />
+      </section>
+    </div>
   );
 }
 
